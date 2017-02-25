131ed5d08e73ec4f8deb3c8f020056cad69588af
Corey Wofford <msucorey@gmail.com>
2017-02-25 07:46:53 -0800

    add hw demos for family member

---
+++ FirstProject/hello.js
@@ -0,0 +1,24 @@
+// puts "hello world"
+console.log("hello world");
+
+function addNumbers() {
+  let n = 0;
+  for (let i = 0; i < 10; i++) {
+    n += i;
+  }
+  console.log(n);
+}
+
+addNumbers();
+
+//
+// def add_num_10
+//   n = 0
+//   10.times do |i|
+//     n += i
+//     puts i
+//   end
+//   puts n
+// end
+//
+// add_num_10
---
+++ FirstProject/hello.rb
@@ -0,0 +1,12 @@
+puts "hello world"
+
+def add_num_10
+  n = 0
+  10.times do |i|
+    n += i
+    puts i
+  end
+  puts n
+end
+
+add_num_10
