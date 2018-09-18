cff57b56162bde28b8203d97e6362139be9967ff
Corey Wofford <msucorey@gmail.com>
2018-09-18 15:26:05 -0400

    Archive old files

--- corey_wofford/app/models/todo_item.rb
+++
@@ -1,6 +0,0 @@
-class TodoItem < ActiveRecord::Base
-
-  validates :body, :todo_list, presence: true
-
-  belongs_to :todo_list
-end
--- corey_wofford/config/routes.rb
+++
@@ -1,5 +0,0 @@
-TodoApp::Application.routes.draw do
-
-  resources :users, :todo_lists, :todo_items
-  resource :session
-end
